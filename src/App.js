4cec8114d320fe1bb4706197960bd7f72f8ee3fc
Tien Pham <tiendev94@gmail.com>
2019-10-06 20:55:41 +0300

    State machine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useMachine } from "@xstate/react";
+import { Machine } from "xstate";
+
+const toggleMachine = new Machine({
+  id: "toggleMachine",
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: {
+        TOGGLE: "active"
+      }
+    },
+    active: {
+      on: {
+        TOGGLE: "inactive"
+      }
+    }
+  }
+});
 
 function App() {
+  const [current, send] = useMachine(toggleMachine);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          send("TOGGLE");
+        }}
+      >
+        Toggle
+      </button>
+
+      {current.matches("active") && <span>We are active</span>}
+      {current.matches("inactive") && <span>We are inactive</span>}
     </div>
   );
 }
